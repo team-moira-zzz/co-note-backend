a155df1ed36a65799f9b8498270f18d74665edea
hunzzzzz <hun819.st@gmail.com>
2025-09-27 20:11:39 +0900

    프로젝트 초기 파일 업로드

---
+++ src/main/java/com/moira/conotebackend/CoNoteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.moira.conotebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoNoteBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoNoteBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moira/conotebackend/CoNoteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moira.conotebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoNoteBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
